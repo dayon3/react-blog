01b09748c375c8595565c130c258be00a2f8fd6e
dayon3 <aliasdayone@gmail.com>
2020-06-30 12:15:14 +0300

    initial commit

---
+++ my-blog-backend/src/build/precache-manifest.6079e14baf8d09baf635a5aeb2d48c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507ff8e620614d9d2e64e69cee74daad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "098ff2b9c726c43cf778",
+    "url": "/static/css/main.1024e9bc.chunk.css"
+  },
+  {
+    "revision": "d21ef155bd5ebb4ecff4",
+    "url": "/static/js/2.9ae5c94e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9ae5c94e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098ff2b9c726c43cf778",
+    "url": "/static/js/main.bf31dfe9.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
